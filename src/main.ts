8f131195ead8063fe6b8d56464c473620f6e3278
Muhammad Huzaifa <huzaifa031252khan@gmail.com>
2026-07-29 05:54:18 -0700

    trying to fix errors

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,16 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors({ origin: 'http://localhost:3000' });
+
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'https://e-commerce-qebn.onrender.com',
+    ],
+  });
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.PORT ?? 4000);
 }
+
 bootstrap();
